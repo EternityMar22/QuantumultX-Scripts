68bcecd071ad18712513dec1e5b51f2218a908a4
KAI <18595122+EternityMar22@users.noreply.github.com>
2024-08-23 16:46:09 +0800

    Update ticktick.js

--- ticktick.js
+++ ticktick.js
@@ -1,3 +1,17 @@
+
+
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/ticktick\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/EternityMar22/QuantumultX-Scripts/main/ticktick.js
+
+[mitm] 
+
+hostname = ticktick.com
+*******************************/
+
 let obj = JSON.parse($response.body);
 obj.proEndDate = "3030-03-03T00:00:00.000+0000";
 obj.pro = true;
